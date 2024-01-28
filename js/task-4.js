601bb4290064da38c970313efd6b15feb521af0a
Denys Puzikov <denys.puzikov@gmail.com>
2024-01-29 00:18:16 +0100

    task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+  const userEmail = email.value.trim();
+  const userPassword = password.value.trim();
+
+  const user = {
+    email: userEmail,
+    password: userPassword,
+  };
+
+  if (userEmail === '' || userPassword === '') {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  console.log(user);
+
+  form.reset();
+}
